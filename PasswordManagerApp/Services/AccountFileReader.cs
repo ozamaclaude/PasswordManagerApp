89e155ce8cbf7e80bfa9a989fe9a4b9016b9677b
ozamaclaude <budha1001@gmail.com>
2021-01-11 14:45:04 +0900

    password manager

---
+++ PasswordManagerApp/Services/AccountFileReader.cs
@@ -0,0 +1,76 @@
+﻿using PasswordManagerApp.Commons;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManagerApp.Services
+{
+    interface IAccountFileReader
+    {
+        void SayHello();
+        List<Account> ReadFile();
+        void FlushContents();
+        void WriteFile(List<Account> accounts);
+    }
+
+    class AccountFileReader : IAccountFileReader
+    {
+        public void SayHello() { Debug.WriteLine("Hello world!"); }
+
+        public void WriteFile(List<Account> accounts)
+        {
+            var path = Properties.Settings.Default.accountfile_path;
+            using (var fileStream = new StreamWriter(path, true))
+            {
+                accounts.ForEach(x => Write(fileStream, x));
+            }
+            
+        }
+        private void Write(StreamWriter fs, Account at)
+        {
+            fs.WriteLine("{0},{1},{2}", at.AccountTitle, at.UserId, at.Password);
+        }
+
+        public void FlushContents()
+        {
+            var path = Properties.Settings.Default.accountfile_path;
+            using (var fileStream = new FileStream(path, FileMode.Open))
+            {
+                // ファイルサイズを0に設定
+                fileStream.SetLength(0);
+            }
+        }
+
+        public List<Account> ReadFile()
+        {
+            var path = Properties.Settings.Default.accountfile_path;
+            if (File.Exists(path) == false) 
+            {
+                Debug.WriteLine("file not found");
+                return null;
+            }
+            var lines = File.ReadAllLines(path);
+
+            var accounts = new List<Account>();
+
+            foreach (var l in lines)
+            {
+                var splitted = l.Split(',');
+                var cnt = splitted.Length;
+                if (splitted.Length < 3) { continue; }
+                accounts.Add(new Account
+                {
+                    AccountTitle = splitted[0],
+                    UserId = splitted[1],
+                    Password = splitted[2]
+                });
+            }
+
+            return accounts;
+        }
+    }
+}
---
+++ PasswordManagerApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,113 @@
+﻿using PasswordManagerApp.Commons;
+using PasswordManagerApp.Services;
+using Prism.Commands;
+using Prism.Mvvm;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Unity;
+
+namespace PasswordManagerApp.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "アカウント管理ツール";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private string _accountTitle = "";
+        public string AccountTitle
+        {
+            get { return _accountTitle; }
+            set { SetProperty(ref _accountTitle, value); }
+        }
+
+        private string _accountId = "";
+        public string AccountId
+        {
+            get { return _accountId; }
+            set { SetProperty(ref _accountId, value); }
+        }
+
+        private string _accountPassword = "";
+        public string AccountPassword
+        {
+            get { return _accountPassword; }
+            set { SetProperty(ref _accountPassword, value); }
+        }
+
+        private ObservableCollection<Account> _accounts
+            = new ObservableCollection<Account>();
+
+        public ObservableCollection<Account> Accounts
+        {
+            get { return _accounts; }
+            set { SetProperty(ref _accounts, value); }
+        }
+
+        private IUnityContainer _container;
+        private IAccountFileReader _reader;
+
+        public DelegateCommand AddCommand { get; private set; }
+        public DelegateCommand RegisterCommand { get; private set; }
+
+        public MainWindowViewModel(IUnityContainer container)
+        {
+            _container = container;
+            Setup();
+        }
+
+        private void Setup()
+        {
+            AddCommand = new DelegateCommand(AddAccount);
+            RegisterCommand = new DelegateCommand(RegisterAccounts);
+
+            _reader = _container.Resolve<IAccountFileReader>();
+            var readLines = _reader.ReadFile();
+            readLines.ForEach(x => Accounts.Add(new Account
+            {
+                AccountTitle = x.AccountTitle,
+                UserId = x.UserId,
+                Password = x.Password
+            }));
+        }
+
+        private void AddAccount()
+        {
+            if (!IsTargetAccountValid()) { return; }
+            Accounts.Add(new Account
+            {
+                AccountTitle = this.AccountTitle,
+                UserId = this.AccountId,
+                Password = this.AccountPassword
+            });
+            ClearTempolaryAccount();
+        }
+
+        private bool IsTargetAccountValid()
+        {
+            if(this.AccountTitle == "" || this.AccountId == "" ) { return false; }
+            foreach( var ac in Accounts)
+            {
+                // タイトルは一意
+                if(ac.AccountTitle == this.AccountTitle) { return false; }
+            }
+            return true;
+        }
+
+        private void ClearTempolaryAccount()
+        {
+            this.AccountTitle = "";
+            this.AccountId = "";
+            this.AccountPassword = "";
+        }
+
+        private void RegisterAccounts()
+        {
+            _reader.FlushContents();
+            _reader.WriteFile(Accounts.ToList());
+        }
+    }
+}
